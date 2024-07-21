a031e1b57115818549813737029e8c4dc332f43f
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-07-21 19:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 //your JS code here. If required.
+// let date = new Date();
+// console.log(date.getDate());
+// console.log(date.getMonth()+1);
+// console.log(date.getFullYear());
+
+let clockSection = document.getElementById("timer");
+function addZero(n){
+    if(n < 10){
+        return "0"+n;
+    }
+    return n;
+}
+function currentTime(){
+    let date = new Date();
+
+    //time
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let amPM
+
+    if(hours > 12){
+        hours -= 12;
+        amPM = "PM"
+    }else if(hours == 0){
+        hours = 1;
+    }else if(hours < 12){
+        amPM = "AM";
+    }else if(hours == 12){
+        amPM = "PM";
+    }
+    minutes = addZero(minutes);
+    seconds = addZero(seconds);
+
+    //Date
+
+    let month = date.getMonth()+1;
+    let day = date.getDate();
+    let year = date.getFullYear();
+
+    // console.log(day+"/"+month+"/"+year,hours+":"+minutes+":"+seconds,amPM);
+    clockSection.innerHTML = day+"/"+month+"/"+year+" "+hours+":"+minutes+":"+seconds+" "+amPM;
+}
+
+setInterval(currentTime, 1000);
+
+
